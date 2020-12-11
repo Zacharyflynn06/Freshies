de5a75868a9308dc59f3065294acb9dfe03788b9
Zac <zacharyflynn06@gmail.com>
2020-12-11 15:51:55 -0700

    Final

--- lib/cli.rb
+++ lib/cli.rb
@@ -10,7 +10,7 @@ class Freshies::CLI
                     "Steamboat",
                     "Telluride",
                     "Vail",
-                    "Winter Park,"]
+                    "Winter Park"]
             
     $prompt = TTY::Prompt.new
  
